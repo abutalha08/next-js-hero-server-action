37a98e944613a44550dac08e2d970dea6d4ad995
abutalha08 <taufique752@gmail.com>
2026-05-07 21:17:12 +0600

    Creating first server action and send form data to the server action

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,19 +1,19 @@
-// import { AddTask } from '@/components/AddTask';
+import { AddTask } from '@/components/AddTask';
 import TasksCard from '@/components/TasksCard';
-// import { createATask } from '@/lib/actions';
+import { createATask } from '@/lib/actions';
 import { getTasks } from '@/lib/tasks';
-// import { Button } from '@heroui/react';
-// import Link from 'next/link';
+import { Button } from '@heroui/react';
+import Link from 'next/link';
 
 const TasksPage = async() => {
     const tasks = await getTasks();
     return (
         <div>
             <h2>Tasks: {tasks.length}</h2>
-            {/* <AddTask createATask = {createATask}></AddTask>
+            <AddTask createATask = {createATask}></AddTask>
             <Link href ="/tasks/new">
                 <Button variant="secondary">Add Task</Button>
-            </Link> */}
+            </Link>
             <div className='grid grid-cols-3 gap-4 pt-8'>
                 {
                     tasks.map(task => <TasksCard key={task.id} task={task}></TasksCard>)
